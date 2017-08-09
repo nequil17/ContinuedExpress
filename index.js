7f788a0e0d2d56df19000723163091f16a96bf31
Nequil Hill <nequil17@hotmail.com>
2017-08-09 09:12:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var api = require('./api');
+
 var app = express();
 var path = require('path');
 var jsonPath = path.join(__dirname, 'data.json');
